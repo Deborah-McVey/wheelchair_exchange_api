e1f763cbe6e6c024a8fad9bce792d6e2df7900bf
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-13 19:31:44 -0500

    auth jwt sessions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+    before_action :set_post, only: [:update, :destroy, :show]
+    # before_action :authenticate_request
+ 
+   def create
+     post = Post.new(post_params)
+     if post.save
+       render json: post, status: :created
+     else
+       render json: post.errors, status: :unprocessable_entity
+     end
+   end
+ 
+   def update  
+     if @post.update(post_params)
+       render json: @post, status: :ok
+     else
+       render json: @post.errors, status: :unprocessable_entity
+     end
+   end
+ 
+   def destroy
+     if @post.destroy
+       render json: @post.errors, status: :unprocessable_entity
+   end
+   
+   private
+ 
+   def set_post
+     @post = Post.find(params[:id])
+   end
+ 
+   def post_params
+     params.permit(:content, :user_id)
+   end
+ end
